591a0aa83da0be5d049c874570d20a3d3a89e339
Viswajith2003 <viswajithviswa715@gmail.com>
2025-01-19 20:40:51 +0530

    nav updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,48 +1,48 @@
 import React, { useState } from "react";
-import "../CSS/Navbar.css";
 import { Link, NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const navItems = [
+    { name: "Home", path: "/" },
+    { name: "Exercise", path: "/exercise" },
+    { name: "BMI", path: "/bmi" },
+    { name: "Contacts", path: "/contacts" },
+  ];
+
   return (
     <motion.nav
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
-      className="px-32"
+      className=" bg-[#000000] text-white p-4 flex justify-between items-center container"
     >
-      <Link to="/" className="title">
-        <h1 className="text-5xl font-bold ml-8">GetShape</h1>
+      <Link to="/" className="text-4xl font-bold">
+        GetShape
       </Link>
-      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
-        <span></span>
-        <span></span>
-        <span></span>
+      <div className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+        <span className="block w-6 h-1 bg-white mb-1"></span>
+        <span className="block w-6 h-1 bg-white mb-1"></span>
+        <span className="block w-6 h-1 bg-white"></span>
       </div>
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <NavLink to="/">
-            <h5 className="text-xl font-bold">Home</h5>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/exercise">
-            <h5 className="text-xl font-bold">Exercise</h5>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/BMI">
-            <h5 className="text-xl font-bold">BMI-calc</h5>
-          </NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/Contacts">
-            <h5 className="text-xl font-bold">Contacts</h5>
-          </NavLink>
-        </li>
+      <ul
+        className={`md:flex md:items-center md:static absolute bg-[#000000] left-0 transition-all duration-500 ease-in ${
+          menuOpen ? "top-full" : "top-[80px]"
+        }`}
+      >
+        {navItems.map((item) => (
+          <li key={item.name} className="md:ml-8 text-xl my-7 md:my-0">
+            <NavLink
+              to={item.path}
+              className="hover:text-gray-300 duration-500 text-[#fe2020]"
+              activeClassName="text-red-500"
+            >
+              {item.name}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </motion.nav>
   );
