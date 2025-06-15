50f855488c4009ca98e2123a07b70b099da72192
Viswajith2003 <viswajithviswa715@gmail.com>
2025-06-15 23:49:34 +0530

    starting the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar"; // Adjust path as needed
+import { Routes, Route } from "react-router-dom";
+// import Navbar from "./Components/Navbar"
 import Home from "./Pages/Home";
 import Exercise from "./Pages/Exercise";
 import Contact from "./Pages/Contacts";
@@ -8,17 +8,14 @@ import Bmi from "./Pages/BMI";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/exercise" element={<Exercise />} />
-          <Route path="/bmi" element={<Bmi />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise" element={<Exercise />} />
+        <Route path="/bmi" element={<Bmi />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 }
 
